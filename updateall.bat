a9adecbdb69177d95dc5484a77dc8c2ab1d2e210
Brian Sadler <brian.sadler@beyondz.org>
2016-09-07 15:18:15 -0400

    Make docker config for just Braven Help and add Canvas Analytics repo

--- updateall.bat
+++ updateall.bat
@@ -11,6 +11,8 @@ echo "Updating canvas-lms  ------------- "
 (cd canvas-lms; git checkout bz-staging; git pull upstream bz-staging; git push origin bz-staging;)
 echo "Updating canvas-lms-js-css  ------------- "
 (cd canvas-lms-js-css; git checkout staging; git pull upstream staging; git push origin staging;)
+echo "Updating canvas analytics  ------------- "
+(cd analytics; git checkout master; git pull upstream master; git push origin master;)
 echo "Updating osqa  ------------- "
 (cd osqa; git checkout master; git pull upstream master; git push origin master;)
 echo "Updating development ------------- "
