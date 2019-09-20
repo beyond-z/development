9a4344f9a3de58d5852fe1b4672746aeb3021a09
Brian Sadler <brian.sadler@beyondz.org>
2019-09-20 14:15:07 -0400

    Remove a bunch of obsolete scripts and docker-compose files to clean this up for dev with the new env.

--- startall.bat
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$(uname)" == "Darwin" ]; then
-  # OS X platform
-  echo "Starting you local development environment for Mac"
-  dinghy up
-fi
-
-docker-compose -f ./docker-compose.yml -f docker-compose-optional.yml up -d || { echo >&2 "Error: docker-compose -f ./docker-compose.yml -f docker-compose-optional.yml up failed."; exit 1; }
